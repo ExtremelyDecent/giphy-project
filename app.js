cf9e32b3e8caade002d002425e3e593cb1093d8b
Henry Frost <henry.90.frost@gmail.com>
2022-06-21 15:34:30 -0400

    working code

--- app.js
+++ app.js
@@ -1,1 +1,37 @@
-console.log("Let's get this party started!");
+const $gifSpace = $("#gifs")
+const $formInput = $("#entry");
+
+function placeGif(r){
+    
+    const numGifs = r.data.length;
+    if(numGifs){
+        let random = Math.floor(Math.random() * r.data.length);
+        let $col = $("<div>", {class: "col-md col-12"});
+        let $gif = $("<img>", {
+            src : r.data[random].images.original.url,
+            class: "w-200"
+        });
+        $col.append($gif);
+        $gifSpace.append($col);
+    }
+    
+}
+
+//gets input
+
+$("form").on("submit", async function (e){
+    e.preventDefault();
+    let search = $formInput.val();
+    $formInput.val("");
+    const r = await axios.get("http://api.giphy.com/v1/gifs/search", {
+        params:{
+            q : search,
+            api_key: "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym"}
+    });
+    console.log(r.data);
+    placeGif(r.data);
+});
+
+$("#remove").on("click", function(){
+    $gifSpace.empty();
+})
